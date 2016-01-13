6f5a761094e131fadb1295946280e001d94ddf8d
DenzylFontaine <ddenzfong@gmail.com>
2016-01-13 16:42:18 -0700

    Changed the background

--- about-me.php
+++ about-me.php
@@ -10,7 +10,7 @@
 		<title></title>
 	</head>
 		<body>
-			<p1 class="dg-green">I am from Dominica </p1>
+			<p1 class="bg-green">I am from Dominica </p1>
 			<br/>
 			<img src="http://www.safehavenrealestate.com/images/dominica_map.jpg" hieght="400" width="400">
 			<p2 class="bg-green">but West Virginia is more like home to me. Maybe it's just i was old enough to start remember things i did there.
