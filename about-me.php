e5085f1ecfc9948d1a39fc17ac106139c29444db
DenzylFontaine <ddenzfong@gmail.com>
2016-01-13 16:53:43 -0700

    Changed the background

--- about-me.php
+++ about-me.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
-	<html lang="en">
+<html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<h1>
-			<strong>This one is about me, myself and I</strong>
-			<br/>
-			<strong>Denzyl Dane Fontaine</strong>
-		</h1>
+		<link href="style.css" rel="stylesheet">
 		<title></title>
 	</head>
-		<body>
-			<p1 class="bg-green">I am from Dominica </p1>
-			<br/>
-			<img src="http://www.safehavenrealestate.com/images/dominica_map.jpg" hieght="400" width="400">
-			<p2 class="bg-green">but West Virginia is more like home to me. Maybe it's just i was old enough to start remember things i did there.
-				Either way ill be a Mountaineer for life!</p2>
-		<div>
-			<img src="http://ezdzine.com/blog/wp-content/uploads/2013/12/wvu" hieght="250" width="250"
-		</div>
-		</body>
+	<body>
+		<strong>This one is about me, myself and I</strong>
+		<br/>
+		<strong>Denzyl Dane Fontaine</strong>
+		<p class=>I am from Dominica </p>
+		<br/>
+		<img class="map" src="http://www.safehavenrealestate.com/images/dominica_map.jpg">
+		<p>but West Virginia is more like home to me. Maybe it's just i was old enough to start remember things i did there.
+			Either way ill be a Mountaineer for life!</p>
+	<div>
+		<img src="http://ezdzine.com/blog/wp-content/uploads/2013/12/wvu">
+	</div>
+	</body>
 </html>
 
