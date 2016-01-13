c8d35e6ae811ed59669d78e9df1120e70125fe87
DenzylFontaine <ddenzfong@gmail.com>
2016-01-13 13:14:46 -0700

    Making a site about myself

---
+++ about-me.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta>
+		<title>This one is about me, myself and I. Denzyl Dane Fontaine</title>
+	</head>
+	<body>
+		<div class="bg-blue" id="paragraph-1">
+<p>This is where im from. I made the background blue because we're surrounded by water.</p>
+		</div>
+<img
+	</body>
+</html>
