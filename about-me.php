8f9e861eff3da322ed992b2c228852fe23c94a57
DenzylFontaine <ddenzfong@gmail.com>
2016-01-14 08:17:30 -0700

    added text color

--- about-me.php
+++ about-me.php
@@ -6,25 +6,25 @@
 		<link href="style.css" rel="stylesheet" type="text/css" />
 		<title></title>
 	</head>
-	<h1>Me Myself and I</h1>
+	<h1>Me Myself and I. Denzyl Dane Fontaine</h1>
 	<body>
 		<div class="bg-purp" id="paragraph-1">
 			<p>Back home to me is the black sandy beaches of Dominica. This island is one of a kind,
 			"the Nature Island of the Caribbean", is where I grew up.
 			Then it was off to West Virginia the home of the WVU Mountaineers.
-			All me and my friends back then wanted to do was be on tha team one day.
-			Ball was life football during any freetime available,then basketball once it got absolutely too cold.
+			All me and my friends back then wanted to do was be on that team one day.
+			Ball was life, football during any freetime available, then basketball once it got absolutely too cold.
 			Some how I ended up in the desert,Abq, New Mexico.
 			Overtime i've learning love this place. Never realized how diverse the places i've lived in are. </p>
 		</div>
 		<img class="pic" src="http://thedominican.net/images/article_images/victoriafalls.jpg"
 			<br/>
 		<img class="pic" src="http://cdn2.hubspot.net/hub/138426/file-17653294-jpg/images/roseau-dominica-news-online"
-			<div class="bdr-purp" id="paragraph-2">
-				<p>Sports caster should be my second job.</p>
-				<p>At least for basketball and football.</p>
-				<p>I practically do that already while watching the games.</p>
-			</div>
+		<div class="bdr-purp" id="paragraph-2">
+			<p>Sports caster should be my second job.</p>
+			<p>At least for basketball and football.</p>
+		<p>I practically do that already while watching the games.</p>
+		</div>
 	</body>
 </html>
 
