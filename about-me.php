5ab3407beee12dd09895233662c51a35b83aba48
DenzylFontaine <ddenzfong@gmail.com>
2016-01-13 23:37:35 -0700

    updated css

--- about-me.php
+++ about-me.php
@@ -8,10 +8,13 @@
 	</head>
 	<body>
 		<div class="bg-purp" id="paragraph-1">
-			<p>Back home to me is the black sandy beaches of Dominica. This one of a kind island, "the Nature Island of the Caribbean", is where I grew up.
-			Then it was off to West Virginia the home of the WVU Mountaineers. All me and my friends back then wanted to do was be on tha team one day.
-			Ball was life football during any freetime available,then basketball once it got absolutely too cold. Some how I ended up in the desert,
-			Abq, New Mexico. Overtime i've learning love this place. Never realized how diverse the places i've lived in are. </p>
+			<p>Back home to me is the black sandy beaches of Dominica. This island is one of a kind,
+			"the Nature Island of the Caribbean", is where I grew up.
+			Then it was off to West Virginia the home of the WVU Mountaineers.
+			All me and my friends back then wanted to do was be on tha team one day.
+			Ball was life football during any freetime available,then basketball once it got absolutely too cold.
+			Some how I ended up in the desert,Abq, New Mexico.
+			Overtime i've learning love this place. Never realized how diverse the places i've lived in are. </p>
 		</div>
 	</body>
 </html>
