fba7045c46a56227cd31bd44b80498519c9fc357
DenzylFontaine <ddenzfong@gmail.com>
2016-01-13 22:39:48 -0700

    resized pictures

--- about-me.php
+++ about-me.php
@@ -11,11 +11,11 @@
 		<strong>Denzyl Dane Fontaine</strong>
 		<p class=>I am from Dominica </p>
 		<br/>
-		<img class="map" src="http://www.safehavenrealestate.com/images/dominica_map.jpg">
+		<img src="http://www.safehavenrealestate.com/images/dominica_map.jpg">
 		<p>but West Virginia is more like home to me. Maybe it's just i was old enough to start remember things i did there.
 			Either way ill be a Mountaineer for life!</p>
 	<div>
-		<img class="man" src="http://ezdzine.com/blog/wp-content/uploads/2013/12/wvu">
+			<img class="wvu" src="http://ezdzine.com/blog/wp-content/uploads/2013/12/wvu">
 	</div>
 	</body>
 </html>
