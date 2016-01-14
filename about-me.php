e552ebd08b939d7a4192d0fbbec97c8c4d0c6fae
DenzylFontaine <ddenzfong@gmail.com>
2016-01-13 23:24:00 -0700

    fresh start

--- about-me.php
+++ about-me.php
@@ -2,21 +2,17 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<link href="style.css" rel="stylesheet">
+		<-- css starts here -->
+		<link href="style.css" rel="stylesheet" href="/css/master.css" type="text/css" />
 		<title></title>
 	</head>
 	<body>
-		<strong>This one is about me, myself and I</strong>
-		<br/>
-		<strong>Denzyl Dane Fontaine</strong>
-		<p class=>I am from Dominica </p>
-		<br/>
-		<img class="map" rc="http://www.safehavenrealestate.com/images/dominica_map.jpg">
-		<p>but West Virginia is more like home to me. Maybe it's just i was old enough to start remember things i did there.
-			Either way ill be a Mountaineer for life!</p>
-	<div>
-			<img class="wvu" src="http://ezdzine.com/blog/wp-content/uploads/2013/12/wvu">
-	</div>
+		<div class="bg-purp" id="paragraph-1">
+			<p>Back home to me is the black sandy beaches of Dominica. This one of a kind island, "the Nature Island of the Caribbean", is where I grew up.
+			Then it was off to West Virginia the home of the WVU Mountaineers. All me and my friends back then wanted to do was be on tha team one day.
+			Ball was life football during any freetime available,then basketball once it got absolutely too cold. Some how I ended up in the desert,
+			Abq, New Mexico. Overtime i've learning love this place. Never realized how diverse the places i've lived in are. </p>
+		</div>
 	</body>
 </html>
 
